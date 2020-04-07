47a85af0508ad056b9f2a97542b239074b1f7727
Dietmar Glachs <dglachs@gmail.com>
2020-04-07 09:53:48 +0200

    also read the contained submodels with direct read

--- src/main/java/at/srfg/iot/indexing/service/impl/AssetServiceImpl.java
+++ src/main/java/at/srfg/iot/indexing/service/impl/AssetServiceImpl.java
@@ -1,14 +1,21 @@
 package at.srfg.iot.indexing.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import at.srfg.indexing.asset.AssetType;
+import at.srfg.indexing.asset.SubmodelType;
 import at.srfg.iot.indexing.service.AssetService;
 import at.srfg.iot.indexing.service.event.SubmodelAwareEvent;
-import at.srfg.iot.indexing.service.impl.SolrServiceImpl;
+import at.srfg.iot.indexing.service.repository.SubmodelRepository;
 
 @Service
 public class AssetServiceImpl extends SolrServiceImpl<AssetType> implements AssetService{
+	@Autowired
+	private SubmodelRepository submodelRepo;
+	
 	@Override
 	public String getCollection() {
 		return AssetType.COLLECTION;
@@ -18,11 +25,24 @@ public class AssetServiceImpl extends SolrServiceImpl<AssetType> implements Asse
 	public Class<AssetType> getSolrClass() {
 		return AssetType.class;
 	}
-
+	/**
+	 * Persist submodels contained in the assettype
+	 */
 	@Override
 	protected void prePersist(AssetType t) {
 		// send event
 		getEventPublisher().publishEvent(new SubmodelAwareEvent(this, t));
 	}
-
+	/**
+	 * On single select - read the associated submodels and load 
+	 * with the asset
+	 */
+	protected void postSelect(AssetType t) {
+		super.postSelect(t);
+		// read the contained submodels
+		List<SubmodelType> subList = submodelRepo.findByAsset(t.getUri());
+		for (SubmodelType sub : subList) {
+			t.addSubmodel(sub);
+		}
+	}
 }
