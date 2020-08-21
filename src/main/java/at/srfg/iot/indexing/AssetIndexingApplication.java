50470acbf405bafbc1ce648e7eefa67cd206616b
Dietmar Glachs <dglachs@gmail.com>
2020-08-21 16:15:40 +0200

    added & activated feign clients

--- src/main/java/at/srfg/iot/indexing/AssetIndexingApplication.java
+++ src/main/java/at/srfg/iot/indexing/AssetIndexingApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.actuate.autoconfigure.security.servlet.Managemen
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @RestController
 @EnableSwagger2
 @EnableAsync
+@EnableFeignClients
 public class AssetIndexingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AssetIndexingApplication.class, args);
