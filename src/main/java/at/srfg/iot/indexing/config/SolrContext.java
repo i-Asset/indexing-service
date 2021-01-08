02525d7cd353204b776020b036e2fa28b18b5464
Dietmar Glachs <dglachs@gmail.com>
2021-01-08 14:18:41 +0100

    adjust @EnableSolrRepositories to changed package structure

--- src/main/java/at/srfg/iot/indexing/config/SolrContext.java
+++ src/main/java/at/srfg/iot/indexing/config/SolrContext.java
@@ -10,9 +10,13 @@ import org.springframework.data.solr.repository.config.EnableSolrRepositories;
 @Configuration
 @EnableSolrRepositories(basePackages= {
 			// enable core SOLR repositories [ClassType | PropertyType | CodedType]Repository
-			"at.srfg.common.solr.indexing.core.service.repository",
+			"at.srfg.iot.common.solr.indexing.core.service.repository",
 			// enable asset SOLR repositories
-			"at.srfg.iot.indexing.service.repository"
+			"at.srfg.iot.indexing.service.repository",
+			// TODO: remove again - enable (temporary) HMA Sensor Playground Repository
+			//       
+			"at.srfg.iot.indexing.service.playground.repository",
+
 		}, 
 		schemaCreationSupport=true)
 public class SolrContext extends at.srfg.iot.common.solr.indexing.core.solr.SolrContext {
